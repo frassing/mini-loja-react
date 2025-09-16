58b9a65400b0ab6c21a4a12b0c00192eb9b1d5d6
Raquel Roessing <raquel.f.roessing@hotmail.com>
2025-09-16 02:00:38 -0400

    feat: add lazy loading attribute to img, use toLocaleString to format price to BRL currency

--- 01-css-global/src/components/ProductCard/ProductCard.jsx
+++ 01-css-global/src/components/ProductCard/ProductCard.jsx
@@ -3,7 +3,7 @@ import Skeleton from '../Skeleton/Skeleton'
 
 export default function ProductCard({product, handleAddToCart, loading}){
 	const formatPrice = (price) => {
-		return price.toString().replace('.', ',')
+		return price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
 	}
 
 	if (loading) {
@@ -12,11 +12,11 @@ export default function ProductCard({product, handleAddToCart, loading}){
 		return (
 		<article className="product-card--container">
 			<picture className='product-card--picture'>
-				<img src={product.image} alt={product.name}/>
+				<img src={product.image} alt={product.name} loading='lazy'/>
 			</picture>
 			<section className='product-card--content'>
 				<h3 className='product-card--name'>{product.name}</h3>
-				<p className='product-card--price'>R$ {formatPrice(product.price)}</p>
+				<p className='product-card--price'>{formatPrice(product.price)}</p>
 				<div className='product-card--rating'>
 					{[...Array(5)].map((_, index) => 
 						<span className='product-card--rating-stars' key={index}>{index < Math.floor(product.rating) ? '★' :'☆' }</span>
