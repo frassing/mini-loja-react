72653f49abdafe939f05264cb5347aac58f91fc2
Raquel Roessing <raquel.f.roessing@hotmail.com>
2025-09-18 16:41:55 -0400

    feat: 03-tailwind create Product card component, create products section, make products grid responsive, add ARIA attributes

--- 03-tailwind/src/App.jsx
+++ 03-tailwind/src/App.jsx
@@ -1,11 +1,19 @@
 import Footer from "./components/Footer"
 import NavBar from "./components/NavBar"
+import ProductCard from "./components/ProductCard"
+import products from "./data/products.json"
 
 function App() {
 
   return (
     <div className="bg-zinc-100 flex flex-col justify-between min-h-screen">
       <NavBar/>
+      <section className="m-2 mb-8 flex flex-col items-center">
+        <h2 className="text-center text-2xl font-bold m-2 text-zinc-950">Produtos</h2>
+        <div className="grid gap-3 grid-cols-1 sm:grid-cols-2 sm:gap-x-6 md:grid-cols-3 lg:grid-cols-4 w-11/12 2xl:w-9/12 justify-items-center">
+          {products.map(p => <ProductCard key={p.id} product={p}/>)}
+        </div>
+      </section>
       <Footer/>
     </div>
   )
--- 03-tailwind/src/components/NavBar.jsx
+++ 03-tailwind/src/components/NavBar.jsx
@@ -1,14 +1,14 @@
 import LogoBlack from '../assets/logo-black.png'
 
 const NavBar = () => {
-	return <header className="sticky top-0 flex items-center justify-between py-1.5 px-3 backdrop-blur-sm bg-linear-to-t from-transparent to-emerald-100 border-b border-b-emerald-100">
+	return <header className="sticky top-0 z-1 flex items-center justify-between py-1.5 px-3 backdrop-blur-sm bg-linear-to-t from-transparent to-emerald-100 border-b border-b-emerald-100">
 		<img src={LogoBlack} alt="Logo RFR" className='max-w-9'/>
 
 		<button type="button" className="cursor-pointer rounded-sm p-1 border border-emerald-600 text-base  text-emerald-800 hover:border-emerald-700 hover:text-emerald-900 transition-colors">Ativar tema escuro</button>
 
 		<div className="flex gap-1 items-center text-xl text-emerald-900">
-			<span>🛒</span>
-			<span>0</span>
+			<span aria-hidden='true'>🛒</span>
+			<span aria-label='Itens no carrinho'>0</span>
 		</div>
 
 	</header>
